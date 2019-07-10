9a7ce46a07cdab7fe684668dd9b8a2b45f1bc74a
NeerNim <neernimt@gmail.com>
2019-07-11 00:13:53 +0200

    replace controller actions with resources

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  resources :static_pages
 end
